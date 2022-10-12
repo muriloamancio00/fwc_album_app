33c659249318bccfefa5df156e07934029655968
murilo <61051754+muriloamancio00@users.noreply.github.com>
2022-10-12 01:03:53 -0300

    AcessoBackEnd-ViaDIO

---
+++ lib/app/core/rest/custom_dio.dart
@@ -0,0 +1,25 @@
+//usado para  customizar algumas coisas do dio, simples
+import 'package:dio/dio.dart';
+import 'package:dio/native_imp.dart';
+
+import '../config/env/env.dart';
+
+//trás informações do back e ele pode sim receber uma informação vazia
+class CustomDio extends DioForNative {
+  CustomDio()
+      : super(BaseOptions(
+            baseUrl: Env.i['backend_base_url'] ?? '',
+            //tempo para chegar ate o backend
+            connectTimeout: 5000,
+            receiveTimeout: 60000)) {
+    interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
+  }
+
+  CustomDio auth() {
+    return this;
+  }
+
+  CustomDio unauth() {
+    return this;
+  }
+}
